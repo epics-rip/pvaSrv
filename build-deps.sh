57e7fa959dcc3e8d1dfe4dabb0c1ee9400ac11a8
Michael Davidsaver <mdavidsaver@gmail.com>
2017-07-23 12:05:04 +0200

    travis-ci add clang mingw builds

--- build-deps.sh
+++ build-deps.sh
@@ -14,10 +14,10 @@ cat configure/RELEASE.local
 install -d "$HOME/.source"
 cd "$HOME/.source"
 
-git clone --quiet --depth 5 --branch "$BRBASE" https://github.com/epics-base/epics-base.git epics-base
-git clone --quiet --depth 5 --branch "$BRPVD" https://github.com/epics-base/pvDataCPP.git pvDataCPP
-git clone --quiet --depth 5 --branch "$BRPVA" https://github.com/epics-base/pvAccessCPP.git pvAccessCPP
-git clone --quiet --depth 5 --branch "$BRNT" https://github.com/epics-base/normativeTypesCPP.git normativeTypesCPP
+git clone --quiet --depth 5 --branch "${BRBASE:-master}" https://github.com/epics-base/epics-base.git epics-base
+git clone --quiet --depth 5 --branch "${BRPVD:-master}" https://github.com/epics-base/pvDataCPP.git pvDataCPP
+git clone --quiet --depth 5 --branch "${BRPVA:-master}" https://github.com/epics-base/pvAccessCPP.git pvAccessCPP
+git clone --quiet --depth 5 --branch "${BRNT:-master}" https://github.com/epics-base/normativeTypesCPP.git normativeTypesCPP
 
 (cd epics-base && git log -n1 )
 (cd pvDataCPP && git log -n1 )
@@ -25,6 +25,19 @@ git clone --quiet --depth 5 --branch "$BRNT" https://github.com/epics-base/norma
 (cd normativeTypesCPP && git log -n1 )
 
 EPICS_HOST_ARCH=`sh epics-base/startup/EpicsHostArch`
+
+# requires wine and g++-mingw-w64-i686
+if [ "$WINE" = "32" ]
+then
+  echo "Cross mingw32"
+  sed -i -e '/CMPLR_PREFIX/d' epics-base/configure/os/CONFIG_SITE.linux-x86.win32-x86-mingw
+  cat << EOF >> epics-base/configure/os/CONFIG_SITE.linux-x86.win32-x86-mingw
+CMPLR_PREFIX=i686-w64-mingw32-
+EOF
+  cat << EOF >> epics-base/configure/CONFIG_SITE
+CROSS_COMPILER_TARGET_ARCHS+=win32-x86-mingw
+EOF
+fi
 
 case "$CMPLR" in
 clang)
